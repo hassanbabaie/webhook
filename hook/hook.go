75acadfd76c4c23680da49810b7a00fff341ebaf
Hass_SEA <hassan@babaie.co.uk>
2018-02-24 12:28:40 -0800

    Change Scalr Signature Match from Error to Validation Fail
    Change Scalr Signature Match from Error to Validation Fail. This is to test if it resolved the OR fail and produces a good output

--- hook/hook.go
+++ hook/hook.go
@@ -146,7 +146,8 @@ func CheckScalrSignature(headers map[string]interface{}, body []byte, signingKey
  	expectedSignature := hex.EncodeToString(mac.Sum(nil))
  
  	if !hmac.Equal([]byte(providedSignature), []byte(expectedSignature)) {
- 		return false, &SignatureError{providedSignature}
+ 		return false, fmt.Errorf("invalid Scalr Signature passed to webhook '%s'", {providedSignature})
+		// return false, &SignatureError{providedSignature}
  	}
 
 	if !checkDate {
